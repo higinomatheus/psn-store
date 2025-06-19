59a944302f97e6927279eda5ea7262dd1a6b955e
Matheus Higino <matheus.higino@yahoo.com.br>
2025-06-19 09:31:01 -0300

    criando o card pricing

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardLabelComponent } from './card-label/card-label.component';
+import { CardPricingComponent } from './card-pricing/card-pricing.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CardLabelComponent],
+  imports: [CardLabelComponent, CardPricingComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
